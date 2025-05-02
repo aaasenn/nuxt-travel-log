ce0333c3ee6f85f40395903c758b82034e830a18
aaasenn <johnsmalest@gmail.com>
2025-05-02 19:40:19 +0300

    add tailwind daysy and icon libs

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="border border-solid border-red-500">
+      Hello world
+    </div>
+    <button class="btn btn-secondary">
+      Click me
+      <Icon name="tabler:map" />
+    </button>
   </div>
 </template>

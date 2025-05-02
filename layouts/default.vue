adeb1b2de06882510b5a4cc29cc9188e256c62f8
aaasenn <johnsmalest@gmail.com>
2025-05-02 21:05:22 +0300

    realize theme toggler

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <div class="navbar bg-primary text-primary-content">
-      <div class="navbar-start">
-        <NuxtLink to="/" class=" btn btn-ghost text-xl">
-          Travel log
-        </NuxtLink>
-      </div>
-      <div class="navbar-end">
-        <a class="btn btn-accent">Sign in</a>
-      </div>
-    </div>
+    <AppNavbar />
     <main>
       <slot />
     </main>

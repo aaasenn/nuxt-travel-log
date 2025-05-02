b252a7e80c571883e451cb0bc58885578ee30717
Sen <91421672+aaasenn@users.noreply.github.com>
2025-05-02 21:07:27 +0300

    Pnd toggle theme (#3)
    * realize theme toggler
    
    * add margin to toggler

---
+++ components/app/navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class=" btn btn-ghost text-xl">
+        Travel log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <AppToggleTheme />
+      <a class="btn btn-accent">Sign in</a>
+    </div>
+  </div>
+</template>
---
+++ components/app/toggle-theme.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark';
+  },
+  set(value) {
+    colorMode.preference = value ? 'dark' : 'light';
+  },
+},
+);
+</script>
+
+<template>
+  <label class="swap swap-rotate mr-6">
+    <!-- this hidden checkbox controls the state -->
+    <input v-model="isDark" type="checkbox">
+
+    <!-- sun icon -->
+    <Icon
+      name="tabler:sun"
+      class="swap-on fill-current"
+      size="24"
+    />
+    <Icon
+      name="tabler:moon"
+      class="swap-off fill-current"
+      size="24"
+    />
+  </label>
+</template>

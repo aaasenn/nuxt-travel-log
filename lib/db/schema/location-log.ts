c17e8f5931c4b860480895b0780ae9ef70f6f182
aaasenn <johnsmalest@gmail.com>
2025-05-09 16:15:02 +0300

    add auth schema

--- lib/db/schema/location-log.ts
+++ lib/db/schema/location-log.ts
@@ -1,5 +1,6 @@
 import { int, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
+import { user } from './auth';
 import { location } from './location';
 
 export const locationLog = sqliteTable('locationLog', {
@@ -8,6 +9,7 @@ export const locationLog = sqliteTable('locationLog', {
   name: text().notNull(),
   slug: text().notNull().unique(), // уникальное имя, например, "moscow" оно описывает местоположение
   description: text(),
+  userId: int().notNull().references(() => user.id), // id пользователя, которому принадлежит лог локации
   lat: real().notNull(), // широта
   long: real().notNull(), // долгота
   startedAt: int().notNull(), // время начала

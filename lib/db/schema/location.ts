da73aed10b58524a8b82a9dde52b406ef85280b2
Sen <91421672+aaasenn@users.noreply.github.com>
2025-05-09 16:19:30 +0300

    add auth schema (#6)

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -1,10 +1,13 @@
 import { int, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
+import { user } from './auth';
+
 export const location = sqliteTable('location', {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   slug: text().notNull().unique(), // уникальное имя, например, "moscow" оно описывает местоположение
   description: text(),
+  userId: int().notNull().references(() => user.id), // id пользователя, которому принадлежит локация
   lat: real().notNull(), // широта
   long: real().notNull(), // долгота
   createdAt: int().notNull().$default(() => Date.now()),
